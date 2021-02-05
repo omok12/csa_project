750b638ad0c0ee5f72cf4523e7437d1e9725a364
o <olivermok@gmail.com>
2021-02-05 10:41:25 -0800

    tidied csa_cost

--- helper.py
+++ helper.py
@@ -14,9 +14,9 @@ class CsaSummary:
 
     #find sum of csa and groupby 
     def csa_cost(self, df, sort='Commission'):
-        df['total_csa_cost'] = abs(df['Quantity']) * df['CSA Commission']
         df['Total Abs Val Quantity'] = abs(df['Quantity'])
         df['Total Abs Val Trans Net Amt'] = abs(df['Trans Net Amt'])
+        df['total_csa_cost'] = df['Total Abs Val Quantity'] * df['CSA Commission']
         grouped_sum = df.groupby(self.aggregate_column).sum()
         grouped_sum['portion_to_csa'] = grouped_sum['total_csa_cost'] / grouped_sum['Commission']
         grouped_sum['commission_trans'] = grouped_sum['Commission'] / grouped_sum['Total Abs Val Trans Net Amt']
@@ -35,6 +35,7 @@ class CsaSummary:
         plt.savefig(f'./img/{self.aggregate_column}_csa_summary')
         plt.show()
 
+    #formatted some columns to be human readable
     def style_df(self, df):
         percentage = ['portion_to_csa', 'commission_trans', 'csa_trans']
         for c in percentage:
